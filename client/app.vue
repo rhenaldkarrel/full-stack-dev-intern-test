6f6fe81d518cd9cbe57d30d9a89cce438886d593
Rhenald <renaldkarel@gmail.com>
2023-04-14 10:42:56 +0700

    feat: initial navbar and layout

--- client/app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1 class="text-4xl">Test</h1>
-  </div>
-</template>
---
+++ client/components/Navbar.vue
@@ -0,0 +1,5 @@
+<template>
+    <nav class="px-8 py-4 bg-[#00DB81] text-white">
+        <NuxtLink href="/" class="text-3xl font-semibold">Nature Social</NuxtLink>
+    </nav>
+</template>
