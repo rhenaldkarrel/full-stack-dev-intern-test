0127d9b65d62564924e191def67db532cf05401f
Rhenald <renaldkarel@gmail.com>
2023-04-14 11:07:06 +0700

    feat: initial comment item and index page

---
+++ client/components/CommentItem.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="border rounded-lg p-4">
+        <h1 class="text-xl font-semibold">{{ props.username }}</h1>
+        <p>{{ props.comment }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+    const props = defineProps({
+        username: String,
+        comment: String,
+    })
+</script>
---
+++ client/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="max-w-7xl mx-auto border min-h-[calc(100vh_-_68px)] p-8 space-y-4">
+        <CommentSearchForm />
+        <div class="flex flex-col space-y-4">
+            <CommentItem
+                :username="'John Doe'"
+                :comment="'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur, ex maxime quo accusamus soluta quasi. Quos blanditiis, ab aspernatur ea temporibus sapiente illo! Adipisci impedit dolorum, explicabo nesciunt, quos ullam quasi maxime natus, alias eius aspernatur consequuntur perferendis magnam!'"
+            />
+            <CommentItem
+                :username="'John Doe'"
+                :comment="'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur, ex maxime quo accusamus soluta quasi. Quos blanditiis, ab aspernatur ea temporibus sapiente illo! Adipisci impedit dolorum, explicabo nesciunt, quos ullam quasi maxime natus, alias eius aspernatur consequuntur perferendis magnam!'"
+            />
+            <CommentItem
+                :username="'John Doe'"
+                :comment="'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur, ex maxime quo accusamus soluta quasi. Quos blanditiis, ab aspernatur ea temporibus sapiente illo! Adipisci impedit dolorum, explicabo nesciunt, quos ullam quasi maxime natus, alias eius aspernatur consequuntur perferendis magnam!'"
+            />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import CommentSearchForm from "~/components/CommentSearchForm.vue";
+import CommentItem from "~/components/CommentItem.vue";
+</script>
