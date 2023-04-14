29f133615e7d5af07a97f1369fa2692ff01249f6
Rhenald <renaldkarel@gmail.com>
2023-04-14 11:03:00 +0700

    feat: add search form component

---
+++ client/components/CommentSearchForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <form>
+        <div class="relative">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                <button
+                    type="submit"
+                    class="p-1 focus:outline-none focus:shadow-outline"
+                >
+                    <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        viewBox="0 0 24 24"
+                        class="w-6 h-6"
+                    >
+                        <path
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        ></path>
+                    </svg>
+                </button>
+            </span>
+            <input
+                type="search"
+                name="query"
+                class="py-2 text-sm border rounded-full pl-10 pr-2 focus:outline-none w-full max-w-[512px]"
+                placeholder="Search..."
+                autocomplete="off"
+            />
+        </div>
+    </form>
+</template>
